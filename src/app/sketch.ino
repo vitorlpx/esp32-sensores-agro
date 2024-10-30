1a6f1c2bd10dc7af8f00b1eca3aaf39291ed7713
vitorlpx <lopessccp.3k@gmail.com>
2024-10-29 21:02:36 -0300

    feature: adicionando a biblioteca DHT e Ultrasonic, definindo os números dos pinos e inicializando os sensores

--- src/app/sketch.ino
+++ src/app/sketch.ino
@@ -1,3 +1,17 @@
+#include <DHT.h>
+#include <Ultrasonic.h>
+
+#define ECHOPino 18
+#define TRIGPino 5
+#define DHTPino 13
+#define DOPino 12
+#define PIRDPino 17
+
+#define modeloDHT DHT22
+
+DHT dht(DHTPino, modeloDHT);
+Ultrasonic ultrasonic(TRIGPino, ECHOPino);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
