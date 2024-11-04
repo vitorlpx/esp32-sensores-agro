74ab41aabbdbe53d0be435a7a30360e79631b0ca
vitorlpx <lopessccp.3k@gmail.com>
2024-11-04 09:51:48 -0300

    feature: adicionando as lógicas dos métodos do código

--- src/app/sketch.ino
+++ src/app/sketch.ino
@@ -35,22 +35,70 @@ void loop() {
   checarNivelAgua();
   checarClima();
   luminosidadeRecebida();
+
+  delay(10000);
 }
 
 void checarNivelAgua() {
+  int distancia = ultrasonic.read();
+  Serial.print("A distância de líquidos nos reservatórios de água: ");
+  Serial.println(distancia);
+
+  if(nivelAgua < aguaNecessaria) {
+    desativarIrrigacao();
+  } else {
+    ativarIrrigacao();
+  }
 }
 
 void desativarIrrigacao() {
+  Serial.println("Desativando a irrigação...");
 }
 
 void ativarIrrigacao() {
+  Serial.println("Ativando a irrigação...");
 }
 
 void checarClima() {
+  float umidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  if(isnan(temperatura) || isnan(umidade)) {
+    Serial.println("Erro ao ler o DHT.");
+  }
+
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+  Serial.print("Umidade: ");
+  Serial.println(umidade);
+
+  if(umidade < umidadeIdeal || temperatura > temperaturaIdeal) {
+    Serial.println("A umidade ou a temperatura não está ideal!");
+    ativarIrrigacao();
+  } else {
+    Serial.println("A umidade ou a temperatura está ideal.");
+    desativarIrrigacao();
+  }
 }
 
 void detectarInvasao() {
+  boolean presencaDetectada = digitalRead(PIRDPino);
+
+  if(presencaDetectada == true) {
+    Serial.println("Invasão detectada! Ativando alarmes de segurança...");
+  } else {
+    Serial.println("Nenhuma invasão detectada.");
+  }
 }
 
 void luminosidadeRecebida() {
+  int intensidadeLuz = analogRead(DOPino); 
+
+  if(luminosidadeIdeal < intensidadeLuz) {
+    Serial.println("A luminosidade está baixa, a irrigação irá ser ativa...");
+    ativarIrrigacao();
+  } else {
+    Serial.println("A luminosidade está ideal.");
+    desativarIrrigacao();
+  }
 }
